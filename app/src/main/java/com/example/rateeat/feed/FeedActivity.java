e031b9ac8903b385b8bfbe439d151ec5512143c6
Carmel <sombrero3@walla.com>
2022-03-07 15:22:24 +0200

    fragments added

---
+++ app/src/main/java/com/example/rateeat/feed/FeedActivity.java
@@ -0,0 +1,16 @@
+package com.example.rateeat.feed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rateeat.R;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
