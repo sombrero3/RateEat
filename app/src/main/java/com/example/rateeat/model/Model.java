b72ac27ccd883c2222f50f1c766382680272c0d4
Or Madmoni <ormadmoni6@cs.colman.ac.il>
2022-03-08 15:30:51 +0200

    add

--- app/src/main/java/com/example/rateeat/model/Model.java
+++ app/src/main/java/com/example/rateeat/model/Model.java
@@ -6,27 +6,71 @@ import android.os.Looper;
 import androidx.core.os.HandlerCompat;
 import androidx.lifecycle.MutableLiveData;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class Model {
-
     public static final Model instance = new Model();
-    public ModelFirebase modelFirebase = new ModelFirebase();
-    Executor executor = Executors.newFixedThreadPool(1);
-    Handler mainThread = HandlerCompat.createAsync(Looper.getMainLooper());
-    MutableLiveData<List<Review>> ReviewsList = new MutableLiveData<>();
-    MutableLiveData<ReviewListLoadingState> ListLoadingState = new MutableLiveData<>();
+    public ModelFireBase modelFireBase = new ModelFireBase();
+    public Executor executor = Executors.newFixedThreadPool(1);
+    public Handler mainThread = HandlerCompat.createAsync(Looper.getMainLooper());
+    public MutableLiveData<List<Review>> ReviewsList = new MutableLiveData<>();
+    public MutableLiveData<ReviewListLoadingState> reviewListLoadingState = new MutableLiveData<>();
+    public User signedUser;
+
+
+
+    public void getAllReviews() {
+        //   modelFireBase.getAllReviews();
+    }
 
     public enum ReviewListLoadingState {
         loading,
         loaded
     }
 
-    private Model() {
-        ListLoadingState.setValue(ReviewListLoadingState.loaded);
 
+    private Model() {
+        reviewListLoadingState.setValue(ReviewListLoadingState.loaded);
+    }
+
+    public boolean isSignedIn() {
+        return modelFireBase.isSignedIn();
+    }
+    public User getSignedUser() {
+
+        return signedUser;
+    }
+    public interface SetCurrentUserListener {
+        void onComplete(User user);
+    }
+    public void setCurrentUser(SetCurrentUserListener listener) {
+        modelFireBase.setCurrentUser(new SetCurrentUserListener() {
+
+            @Override
+            public void onComplete(User user) {
+                signedUser = user;
+                listener.onComplete(user);
+            }
+        });
+    }
+
+    public interface getUserByIdListener{
+        void onComplete(User user);
+    }
+    public void getUserById(String id,getUserByIdListener listener){
+        modelFireBase.getUserById(id,listener);
+
+    }
+    public interface AddUserListener{
+        void onComplete();
+    }
+    public void addUser(User user,AddUserListener listener) throws JsonProcessingException {
+        reviewListLoadingState.setValue(ReviewListLoadingState.loaded);
+        modelFireBase.addUser(user,listener);
     }
 
 }
