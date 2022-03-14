9cd6cb02e1baa3c8921779ce39a717cde4218582
Carmel <sombrero3@walla.com>
2022-03-14 17:21:55 +0200

    delete view model factory

--- app/src/main/java/com/example/rateeat/feed/UserReviewsFragment.java
+++ app/src/main/java/com/example/rateeat/feed/UserReviewsFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -24,18 +22,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.rateeat.MyApplication;
 import com.example.rateeat.R;
 import com.example.rateeat.adapters.UserReviewAdapter;
 import com.example.rateeat.adapters.OnItemClickListener;
 import com.example.rateeat.model.Model;
 import com.example.rateeat.model.Review;
 import com.example.rateeat.model.User;
-import com.example.rateeat.view_models.GeneralListViewModel;
-import com.example.rateeat.view_models.UserReviewViewModelFactory;
 import com.example.rateeat.view_models.UserReviewsViewModel;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class UserReviewsFragment extends Fragment {
